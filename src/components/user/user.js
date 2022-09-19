d9f6da4c5360ac6c51922f98ceab84737cf02181
alinabezux <alinabezux@gmail.com>
2022-09-19 19:35:27 +0300

    react hw1

--- src/components/user/user.js
+++
@@ -1,9 +0,0 @@
-export default function User(props) {
-    let {item: user} = props;
-    return (
-        <div>
-            <h2>{user.id} {user.name}</h2>
-            <p>{user.address.city} <br/> {user.email}</p>
-        </div>
-    );
-}
