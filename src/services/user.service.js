ff9e84ff271585893435e36d2a0cec71a6845658
alinabezux <alinabezux@gmail.com>
2022-09-27 02:34:13 +0300

    hw2

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,6 +3,10 @@ import {axiosService} from "./axios.service";
 const getUsers = () => {
     return axiosService.get('/users')
 }
+
+const getUser = (id) => {
+    return axiosService.get(getUsers() + id)
+}
 export {getUsers};
 
   
