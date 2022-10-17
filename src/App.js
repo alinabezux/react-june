ec47f8f1820c14552734ae45f6d5f3509c0bf751
alinabezux <alinabezux@gmail.com>
2022-10-17 16:51:08 +0300

    classComponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css'
+// import {ClassComponent} from "./components/ClassComponent";
+import {useState} from "react";
+import {FunctionComponent} from "./components/FunctionComponent";
 
 function App() {
+
+    const [flag, setFlag] = useState(true);
     return (
         <div>
-
+            {flag && <FunctionComponent name='max'/>}
+            <button onClick={() => setFlag(prev => !prev)}>Hide</button>
         </div>
     )
 }
---
+++ src/components/FunctionComponent.js
@@ -0,0 +1,38 @@
+import {useEffect, useState} from "react";
+
+const FunctionComponent = ({name}) => {
+    console.log('constructor');
+    const [state, setState] = useState({a: 0, b: 25});
+
+    useEffect(() => {
+        console.log('componentDidMount');
+        return () => {
+            console.log('componentWillUnmount')
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log('componentDidUpdate');
+    }, [state])
+
+    const inc = () => {
+        setState(prev => ({...state, a: state.a + 1}))
+    }
+
+
+    return (
+        <div>
+            {
+                console.log('render')
+            }
+            FunctionComponent
+            <div>A:{state.a}</div>
+            <div>B:{state.b}</div>
+            <div>Props:{name}</div>
+            <button onClick={() => inc()}>+ 1</button>
+
+        </div>
+    );
+}
+
+export {FunctionComponent}
