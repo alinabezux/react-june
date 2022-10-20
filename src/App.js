44f3dd2e9967b98612075fde673d083aba832e0c
alinabezux <alinabezux@gmail.com>
2022-10-20 21:24:32 +0300

    preview redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css'
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
+
+    const state = useSelector((state) => state);
+    const dispatch = useDispatch();
+
     return (
         <div>
-
+            <h2>{state}</h2>
+            <button onChange={dispatch({type: 'INC', payload: 1})}>increment</button>
+            <button onChange={dispatch({type: 'DEC', payload: 1})}>decrement</button>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
+function reducer(state = 0, action) {
+    switch (action.type) {
+        case 'INC':
+            state = state + action.payload
+            return state;
+        case 'DEC':
+            state = state - action.payload
+            return state;
+        default:
+            return state;
+    }
+}
+
+let store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <Provider store={store}>
             <App/>
-        </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
 
